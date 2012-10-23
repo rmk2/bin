75325ddd87ff10d2342025be1c1535b1ed257c0e
Peter Hanisch <veniastra@gmail.com>
2012-10-23 20:26:46 +0100

    modified script > git-push-remote.sh
    revamped the whole git push script
    
    added: individual functions for better modularity
    added: check whether the current working directory is a git directory
    added: (manual) parameter "--pi" to push to additional target "pi"
    added: meaningful error message if .git doesn't exist in the pwd
    added: dynamic git archive command to generate archives based on the pwd's
    basename instead of a hard-coded value (also stripping leading ".")

--- games.sh
+++ games.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 export WINEDEBUG=-all
--- git-push-remote.sh
+++ git-push-remote.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
 
-if 
-[ `pwd` = '/home/ryko/Documents/TeX/universitet' ]; then
-    echo
-    echo "Pushing to remote repositories"
-    # echo "-------------"
-    # echo "> uni-login"
-    # echo "-------------"
-    # git push ph35
+function push-ox() {
     echo "-------------"
     echo "> ox"
     echo "-------------"
     git push ox HEAD:master
+}
+
+function push-bb() {
     echo "-------------"
     echo "> bitbucket"
     echo "-------------"
     git push bb HEAD:master
+}
+
+function push-pi() {
+    echo "-------------"
+    echo "> pi"
+    echo "-------------"
+    git push pi HEAD:master
+}
+
+function exec-hooks() {
     echo "-------------"
     echo "> executing post-commit hook"
     echo "-------------"
     sh .git/hooks/post-commit
     echo Success
+}
+
+function exec-archive() {
     echo "-------------"
     echo "> running git archive on HEAD"
     echo "-------------"
-    git archive --format=tar.gz HEAD > ~/universitet.tar.gz
+    git archive --format=tar.gz HEAD > ~/$(basename $(pwd) | tr -d '.').tar.gz
     echo Success
+}
+
+if [ $(pwd) = '/home/ryko/Documents/TeX/universitet' ]; then
+    echo
+    echo "Pushing to remote repositories"
+    push-ox
+    push-bb
+    if [ "$1" == "--pi" ]; then
+	push-pi
+    fi
+    exec-hooks
+    exec-archive
+    echo "-------------"
+    echo "Mission accomplished!"
+    echo
+elif [ -d './.git' ]; then
+    echo
+    echo "Pushing to remote repositories"
+    push-ox
+    push-bb
+    if [ "$1" == "--pi" ]; then
+	push-pi
+    fi
     echo "-------------"
     echo "Mission accomplished!"
     echo
 else
-    echo "Wrong directory"
+    echo "Error: This directory is not under version control via git!"
 fi
