d5c3a5465ca8eb1a9913ad095a951d9246e3f2a9
Peter Hanisch <veniastra@gmail.com>
2012-12-05 00:06:56 +0000

    added script > git-create.sh
    This script is intended to help with the automatic creation of remote
    repositories including appropriate folders on the given remote servers.
    
    The usual form of access to remote servers is via ssh, BitBucket is a special
    case in works with reduced functionality (no automatic repository creation).

---
+++ git-create.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+## define DIR and DIR aliases for specific folders (~/ & ~/.bin)
+
+case $(basename $(pwd)) in
+    ryko)
+	DIR="backup/git/config"
+	;;
+    .bin)
+	DIR="backup/git/scripts"
+	;;
+    *)
+	DIR="backup/git/$(basename $(pwd))"
+esac
+
+## define server names
+
+if [ -z "$2" ]; then
+    ARRAY="bb ox pi"
+else
+    ARRAY="$2 $3 $4 $5 $6"
+fi
+
+## define functions
+
+function create() {
+    for i in $ARRAY; do
+	NAME=$i
+	if [ "$i" = "bb" ]; then
+	    echo "Please use the web interface on bitbucket.org to create remote repositories."
+	else
+	    ssh -q $NAME mkdir -p ~/$DIR
+	    ssh -q $NAME git init --bare ~/$DIR/.git
+	fi
+    done
+}
+
+function add() {
+    for i in $ARRAY; do
+	NAME=$i
+	if [ "$i" = "bb" ]; then
+	    git remote add $NAME $NAME:ryko/$(basename $DIR).git
+	else
+	    git remote add $NAME $NAME:$DIR/.git
+	fi
+    done
+}
+
+function push() {
+    for i in $ARRAY; do
+	NAME=$i
+	git push $NAME HEAD:master
+    done
+}
+
+## execution part
+
+case "$1" in
+    -c | --create | create)
+	create
+	;;
+    -a | --add | add)
+	add
+	;;
+    -p | --push | push)
+	push
+	;;
+    -A | --all | all)
+	create
+	add
+	push
+	;;
+    -t | --test)
+	echo $DIR
+	echo $ARRAY
+	;;
+    -h | --help | -? | ? | help)
+	echo 'Usage: $NAME [OPTION] <remote server names>'
+	echo
+	echo Note:
+	echo 'If no remote (space-separated) servers are given, the standard servers are used.'
+	echo -e '\t\t Standard servers: bb, ox, pi'
+	echo -e '\t\t Maximum servers: 5'
+	echo
+	echo Available options:
+	echo -e '-c, --create \t create folders and initialise a bare git repository on the remote server'
+	echo -e '-a, --add \t add remote server as remote git repository'
+	echo -e '-p, --push \t push current git repository into remote git repository'
+	echo -e '-A, --all \t create, then add, then push, as described above, for all given remote servers'
+	;;
+    *)
+	echo Usage: $NAME [OPTION]
+	echo Try $NAME --help for more information.
+esac
