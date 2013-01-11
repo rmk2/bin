9fb37b3807504501f9a154e9492ff135734dceeb
Peter Hanisch <veniastra@gmail.com>
2013-01-11 00:27:38 +0000

    added script > migrate-to-ssd.sh
    This script is meant to automate transferring data from one disk to
    another. The source disk needs to be mounted as /media/hdd, the target disk as
    /media/ssd, the script then takes one argument (data|home|root) and copies the
    corresponding folders from the source disk to the target disk. The copying is
    done using rsync locally.

---
+++ migrate-to-ssd.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+SSD=/media/ssd
+HDD=/media/hdd
+TEST="--list-only"
+
+if [ -e "$HDD" ]; then
+    echo "$HDD found, continuing!"
+else
+    echo "Error: $HDD does not exist!"
+    exit 0
+fi
+
+if [ -e "$SSD" ]; then
+    echo "Warning: $SSD already exists!"
+else
+    echo "Creating $SSD and subfolders!"
+    sudo mkdir -p $SSD/{home,data,dev,media,mnt,proc,sys,tmp,windows}
+fi
+
+case "$1" in
+    data | --data)
+	SOURCE=$HDD/data/
+	TARGET=$SSD/data/
+	rsync -amAXzv --safe-links $TEST \
+	    "$SOURCE" "$TARGET"
+	;;
+    home | --home)
+	SOURCE=$HDD/home/ryko
+	TARGET=$SSD/home/
+	rsync -amAXzv --safe-links $TEST \
+	    --exclude='**arm*' \
+	    --exclude='.adobe**' \
+	    --exclude='.cache**' \
+	    --exclude='.macromedia**' \
+	    --exclude='rpmbuild**' \
+	    --exclude='**.iso' \
+	    "$SOURCE" "$TARGET"
+	;;
+    root | --root)
+	SOURCE=$HDD/
+	TARGET=$SSD/
+	rsync -amHAXzv $TEST \
+	    --exclude='/dev**' \
+	    --exclude='/media**' \
+	    --exclude='/mnt**' \
+	    --exclude='/proc**' \
+	    --exclude='/sys**' \
+	    --exclude='/tmp**' \
+	    --exclude='/var/tmp**' \
+	    --exclude='/home/ryko**' \
+	    --exclude='/data**' \
+	    --exclude='/windows**' \
+	    "$SOURCE" "$TARGET"
+	;;
+    *)
+	exit 0
+esac
