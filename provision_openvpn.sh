d58cef5a8960782e2c2f39331cf63f1c62f03487
Peter Hanisch <veniastra@gmail.com>
2015-06-25 19:18:00 +0100

    added script > provision_openvpn.sh
    This script marks an attempt to somewhat automate the creation (and thus
    provisioning) of new openvpn keys, though it doesn't really do all that much
    as of now.

---
+++ provision_openvpn.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# Initialise easy-rsa and variables
+
+function init() {
+    cd /etc/openvpn/easy-rsa/
+    touch keys/index.txt
+    echo 01 > keys/serial
+    . ./vars # set environment variables
+    ./clean-all
+}
+
+# Generate CA certificate/key
+function exec_ca() {
+    ./build-ca 
+}
+
+# Build intermediate CA certificate/key
+
+function exec_server() {
+    ./build-key-server server
+}
+
+# Generate Diffie-Hellman Parameters
+
+function exec_dh() {
+    ./build-dh
+}
+
+# Generate key for each client (with password)
+
+CLIENTS="ryko thistle nexus"
+
+function exec_client() {   
+    for i in $CLIENTS; do
+	./build-key-pass $i
+    done
+}
+
+# Main
+
+exec_client
