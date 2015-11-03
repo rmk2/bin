e896bb353e9bce977b886b412ccaa8e4084b2a87
Peter Hanisch <peter@rmk2.org>
2015-11-03 14:04:02 +0000

    modified script > eve-git.sh
    added: eve-intel_regions.results as new input
    removed: disable legace output
    changed: push to git-remote edis instead of using a public github repo

--- eve-git.sh
+++ eve-git.sh
@@ -3,15 +3,18 @@
 # eve-git - watch file and git commit all changes as they happen
 #
 
-FILE="/home/ryko/eve-intel.results"
+PLAIN="/home/ryko/eve-intel.results"
+REGIONS="/home/ryko/eve-intel_regions.results"
 DEST="/home/ryko/eve"
 
 while true; do
 
-    inotifywait -qq -e CLOSE_WRITE $FILE
-    <$FILE sort | uniq -c | sed 's/^\s*\([0-9]*\)\s*/\1,/g' > $DEST/results.txt
+    inotifywait -qq -e CLOSE_WRITE $REGIONS
+    # <$PLAIN sort | uniq -c | sed 's/^\s*\([0-9]*\)\s*/\1,/g' > $DEST/results.txt
+    cp $REGIONS $DEST/results_regions.txt
     cd $DEST
     git commit -a -m "autocommit @ $(date -I)"
-    git push origin master
+    # git push origin master
+    git push edis master
 
 done
