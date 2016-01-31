2e9cb43e44f2182cf0a5ef6f43208f0e9e52a89c
Peter Hanisch <veniastra@gmail.com>
2016-01-31 03:10:43 +0100

    modified script > edis-letsencrypt.sh
    added: f.rmk2.org
    added: p.rmk2.org
    changed: moved the letsencrypt command into the case statements, so we
    can use custom webroot paths for each

--- edis-letsencrypt.sh
+++ edis-letsencrypt.sh
@@ -6,10 +6,11 @@ DOMAIN=""
 
 BIN_DIR="/home/ryko/.local/share/letsencrypt/bin"
 SOURCE_DIR="/etc/letsencrypt/live"
+SERVER_DIR="/var/www/servers"
 
 EMAIL="mail@rmk2.org"
 
-DOMAIN_LIST="eve.rmk2.org imap.rmk2.org smtp.rmk2.org"
+DOMAIN_LIST="eve.rmk2.org imap.rmk2.org smtp.rmk2.org f.rmk2.org p.rmk2.org"
 
 echo
 echo "Renewing the following domains: $DOMAIN_LIST"
@@ -21,18 +22,29 @@ for i in $DOMAIN_LIST; do
     echo "> $DOMAIN"
     echo "-------------"
     
-    sudo $BIN_DIR/letsencrypt auth --renew-by-default --email $EMAIL -d $DOMAIN
-
     case $i in
-	"eve.rmk2.org")
+	"eve.rmk2.org" | "p.rmk2.org" | "f.rmk2.org")
+
+	    sudo $BIN_DIR/letsencrypt auth --renew-by-default --email $EMAIL -d $DOMAIN -w $SERVER_DIR/$DOMAIN/pages
 
 	    LIGHTTPD_DIR="/etc/lighttpd"
-	    
+
 	    # Combine key and chain for lighttpd
 	    echo "...concatenating files"
 	    sudo cat $SOURCE_DIR/$DOMAIN/cert.pem $SOURCE_DIR/$DOMAIN/privkey.pem | sudo tee $LIGHTTPD_DIR/ssl/$DOMAIN/server.pem
+
+	    if [ ! -e $LIGHTTPD_DIR/ssl/$DOMAIN/cert.pem ]; then
+		sudo ln -s $SOURCE_DIR/$DOMAIN/cert.pem $LIGHTTPD_DIR/ssl/$DOMAIN/
+	    fi
+
+	    if [ ! -e $LIGHTTPD_DIR/ssl/$DOMAIN/chain.pem ]; then
+		sudo ln -s $SOURCE_DIR/$DOMAIN/chain.pem $LIGHTTPD_DIR/ssl/$DOMAIN/
+	    fi
+	    
 	    ;;
 	"smtp.rmk2.org")
+
+	    sudo $BIN_DIR/letsencrypt auth --renew-by-default --email $EMAIL -d $DOMAIN -w $SERVER_DIR/eve.rmk2.org/pages
 
 	    EXIM_DIR="/etc/exim4"
 	    
@@ -44,6 +56,12 @@ for i in $DOMAIN_LIST; do
 	    # Make sure the exim-user has access
 	    echo "...changing ownership"
 	    sudo chown root:Debian-exim $EXIM_DIR/{exim.crt,exim.key}
+	    
+	    ;;
+	"imap.rmk2.org")
+
+	    sudo $BIN_DIR/letsencrypt auth --renew-by-default --email $EMAIL -d $DOMAIN -w $SERVER_DIR/eve.rmk2.org/pages
+	    
 	    ;;
 	*)
 	    
